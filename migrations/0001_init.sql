7e33bc0a504be567d5c2a984026de11ad8b43cac
thaletto <krlaxman03@gmail.com>
2026-06-05 23:21:41 +0530

    feat: add products schema migration

---
+++ migrations/0001_init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    price REAL,
+    brand TEXT,
+    description TEXT,
+    category TEXT,
+    image TEXT,
+    compare_price REAL
+);
